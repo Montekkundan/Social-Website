1689041e26ce38de7a0e8d7c56cc4a962ab2dab6
Montek <montekkundan@gmail.com>
2022-05-06 00:46:28 +0530

    added login page

---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WeTalks | Login</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Water+Brush&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./login.css">
+</head>
+</style>
+
+<body>
+    <div id="bar">
+        <div class="Title">WeTalks</div>
+        <div class="Subtitle">Login Up</div>
+
+    </div>
+    <div id="bar2">
+        <h2>Login to WeTalks</h2><br>
+        <input type="email" id="text" placeholder="Email address"><br><br>
+        <input type="password" id="text" placeholder="Password"><br><br>
+        <input type="submit" id="button" value="Login"><br><br>
+        <span>Not registered yet? </span><span><a href="./signup.php">Sign up</a></span>
+
+
+    </div>
+</body>
+
+</html>
---
+++ signup.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WeTalks | SignUp</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Water+Brush&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./signup.css">
+</head>
+</style>
+
+<body>
+    <div id="bar">
+        <div class="Title">WeTalks</div>
+        <div class="Subtitle">Sign Up</div>
+
+    </div>
+    <div id="bar2">
+        <h2>Login to WeTalks</h2><br>
+        <input type="text" id="text" placeholder="First Name"><br><br>
+        <input type="text" id="text" placeholder="Last Name"><br><br>
+        <span>Gender:</span> <br>
+        <select id="text">
+            <option disabled selected value> -- select an option -- </option>
+            <option>Male</option>
+            <option>Female</option>
+        </select> <br><br>
+        <input type="email" id="text" placeholder="Email address"><br><br>
+        <input type="password" id="text" placeholder="Password"><br><br>
+        <input type="password" id="text" placeholder="Retype-Password"><br><br>
+        <input type="submit" id="button" value="Sign Up"><br><br>
+        <span>Already registered? </span><span><a href="./login.php">Login in</a></span>
+
+    </div>
+</body>
+
+</html>
